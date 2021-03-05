2bda2dbd06cb594be0d8497d21ac56dbd78e521b
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-05 11:40:02 +0530

    ohh boi, added a lot of code you can check it in products.js in routers whrer we have actullly connected to the mongodb

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,118 @@
 const express = require("express");
 const routes = express.Router()
 
+const Product = require("../models/product")
+const mongoose = require("mongoose");
+const {
+    update
+} = require("../models/product");
 
 routes.get("/", (req, res, next) => {
-    res.status(200).json({
-        messsag: "Handlling GET requests to proucts !"
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs)
+            if (docs.length > 0) {
+                res.status(200).json(docs)
+            } else {
+                res.status(404).json({
+                    message: "Could not found any doc !"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                erro: err
+            })
+        })
+
 })
 
 routes.post("/", (req, res, next) => {
-    const product = {
+
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price : req.body.price
-    }
-    res.status(200).json({
-        messsag: "Handlling POST requests to proucts !",
-        createProduct: product
+        price: req.body.price
+
     })
+    product.save().then((result) => {
+        console.log("fuck:" + result)
+        res.status(200).json({
+            messsag: "Handlling POST requests to proucts !",
+            createProduct: result
+        })
+    }).catch(err => {
+        console.log(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+    })
+
 })
 
 
 
 routes.get("/:productid", (req, res, next) => {
     const id = req.params.productid
-    res.status(200).json({
-        message: "You need a GET product with ID",
-        id_: id
-    })
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc)
+            if (doc) {
+                res.status(200).json(doc)
+            } else {
+                res.status(404).json({
+                    message: "No valid entry found !"
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+
 })
 
-routes.put("/:productid", (req, res, next) => {
+routes.patch("/:productid", (req, res, next) => {
     const id = req.params.productid
-    res.status(200).json({
-        message: "You need a PUT product with ID",
-        id_: id
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+    Product.updateOne({
+        _id: id
+    }, {
+        $set: updateOps
+    }).exec().then(result => {
+        console.log(result)
+        res.status(200).json({
+            result
+        }).catch(err =>{
+            res.status(200).json({
+                error : err
+            })
+        })
     })
 })
 
 routes.delete("/:productid", (req, res, next) => {
     const id = req.params.productid
-    res.status(200).json({
-        message: "You need a DELETE product with ID",
-        id_: id
+    Product.remove({
+        _id: id
+    }).exec().then(
+        result => {
+            res.status(200).json(result)
+        }
+    ).catch(err => {
+        res.status(500).json({
+            error: err
+        })
     })
 })
 
--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const app = express()
 const productRoutes = require("./api/routes/products")
 const ordersRoutes = require("./api/routes/orders")
 
-// just install body parser which parses the URL !
 
+
+// following is the mongo db connection !
+
+const mongoose = require("mongoose")
+mongoose.connect(
+    "mongodb://localhost:27017/NodeAPI"
+)
+
+// above is the mongo db connection !
+
+
+
+// just install body parser which parses the URL !
 // following are the body-parser one time req
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+app.use("/products", productRoutes)
+app.use("/orders", ordersRoutes)
+
 // following is the CORS access !
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'),
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With,Content-Type,Accept,Authorization')
-    
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With,Content-Type,Accept,Authorization')
+
     /* 
         Browser will always send the OPTIONS request first,
         when you send the POST  or PUT request !
@@ -28,15 +44,9 @@ app.use((req, res, next) => {
         })
     }
     next()
- 
+
 })
 // above are the body-parser one time req
-
-
-
-app.use("/products", productRoutes)
-app.use("/orders", ordersRoutes)
-
 
 
 app.use((req,res,next)=>{

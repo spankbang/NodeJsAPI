baae3162c8baa37efb44f78643915a85b0115449
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-05 09:19:16 +0530

    added nodemon, errors handling ,404 and 500 messages

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ const ordersRoutes = require("./api/routes/orders")
 app.use("/products", productRoutes)
 app.use("/orders", ordersRoutes)
 
+app.use((req,res,next)=>{
+    const error = new Error("Not Found !")
+    error.status=404
+    next(error);
+})
 
-// app.use((req, res,next) => {
-//     res.status(200).json({
-//         message : "It works !"
-//     })
-// })
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message : error.message
+        }
+    })
+})
 
 
 module.exports = app

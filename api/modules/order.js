f2c31dcefb2f8e90ca536fcf30f40ad585a26d4b
parimalmahindrakar <parimalm4653@gmail.com>
2021-03-06 21:55:59 +0530

    added the order file with GET, POST, PATCH , DELETE

---
+++ api/modules/order.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const orderschema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    }
+})
+
+module.exports = mongoose.model("Order", orderschema)
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,4 +1,11 @@
 var express = require("express")
+var mongoose = require("mongoose")
+const Order = require("../modules/order")
+const Product = require("../modules/products")
+
+
+
+
 const {
     route
 } = require("./product")
@@ -9,22 +16,72 @@ var router = express.Router()
 // single end points
 
 router.get("/", (req, res, next) => {
-    res.status(200).json({
-        msg: "orders GET"
-    })
+    Order.find().select("_id product quantity").exec()
+        .then(docs => {
+            if (docs.length > 0) {
+                res.status(200).json({
+                    count: docs.length,
+                    docs: docs.map((doc) => {
+                        return {
+                            doc: doc,
+                            type: "GET",
+                            url: "http://127.0.0.1:3000/orders/" + doc._id
+                        }
+                    }),
+
+                })
+            } else {
+                res.status(404).json({
+                    msg: "No documents"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+
 })
 
 
 router.post("/", (req, res, next) => {
-    const order = {
-        productid: req.body.productid,
-        quantity : req.body.quantity
-    }
-    res.status(200).json({
-        msg: "orders POST",
-        order:order
-    })
-})
+    Product.findById(req.body.productId)
+        .then(product => {
+            console.log(product)
+            if (!product) {
+                return res.status(404).json({
+                    message: "Product not found"
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId
+            });
+            return order.save();
+        })
+        .then(result => {
+            res.status(201).json({
+                message: "Order stored",
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders/" + result._id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
 
 // ------------------------------------------
 
@@ -33,10 +90,12 @@ router.post("/", (req, res, next) => {
 
 router.get("/:orderid", (req, res, next) => {
     const id = req.params.orderid
-    res.status(200).json({
-        msg: "orders GET",
-        id: id
-
+    Order.find({
+        _id: id
+    }).then((result) => {
+        res.status(200).json({
+            doc: result
+        })
     })
 })
 
@@ -51,11 +110,18 @@ router.patch("/:orderid", (req, res, next) => {
 
 router.delete("/:orderid", (req, res, next) => {
     const id = req.params.orderid
-    res.status(200).json({
-        msg: "orders DELETE",
-        id: id
-
-    })
+    Order.remove({
+            _id: id
+        }).exec()
+        .then((result) => {
+            res.status(200).json({
+                msg: id + " removed successfully."
+            }).catch(err => {
+                res.status(500).json({
+                    error: err
+                })
+            })
+        })
 })
 
 
